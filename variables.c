d2fa055409fe6640f5a304109629428076e0db72
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2022-09-01 20:39:29 +0530

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+int main()
+{ 
+    int a=4;
+    float b=3.8;
+    char c= 'j';
+
+    printf("tha value of a is  %d \n",a );
+    printf("the value of b is  %f  \n",b);
+    printf("the value of c is  %c  \n",c);
+    printf("the value of b is  %f  \n",a+b);
+    
+    
+    
+    return 0;
+
+}    
+    
